147b06974d035eb314cfbed72f2f0a9c1c3cd2e0
aagudo <aagudo@cifpfbmoll.eu>
2020-11-20 18:46:06 +0100

    punto 7 acabado

--- phpCrud/config/database.php
+++ phpCrud/config/database.php
@@ -1,11 +1,11 @@
 <?php 
-$host = "localhost";
-$db_name = "php_beginner_crud_level_1";
-$username = "aagudo";
-$password = "0076Vaab";
-try{
-    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-}catch(PDOException $exception){
-    echo "Connection error: ".$exception->getMessage();
-}
+    $host = "localhost";
+    $db_name = "php_beginner_crud_level_1";
+    $username = "aagudo";
+    $password = "0076Vaab";
+    try{
+        $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    }catch(PDOException $exception){
+        echo "Connection error: ".$exception->getMessage();
+    }
 ?>
