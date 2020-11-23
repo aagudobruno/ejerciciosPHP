62bc204fbb0d890fc3fcfdc90b7e2fffe5a61e3b
aagudo <aagudo@cifpfbmoll.eu>
2020-11-23 13:10:53 +0100

    punto 10 acabado

--- phpCrud/index.php
+++ phpCrud/index.php
@@ -24,24 +24,30 @@
         <?php
         include 'config/database.php';
         
+        $page = isset($_GET['page']) ? $_GET['page'] : 1;
+
+        $records_per_page = 5;
+
+        $from_record_num = ($records_per_page * $page) - $records_per_page;
+
         $action = isset($_GET['action']) ? $_GET['action'] : "";
         if($action=='deleted'){
             echo "<div class='alert alert-success'>Record was deleted.</div>";
         }
 
-        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC";
+        $query = "SELECT id, name, description, price FROM products ORDER BY id DESC
+        LIMIT :from_record_num, :records_per_page";
         $stmt = $con->prepare($query);
-        $stmt->execute();        
+        $stmt->bindParam(":from_record_num", $from_record_num, PDO::PARAM_INT);
+        $stmt->bindParam(":records_per_page", $records_per_page, PDO::PARAM_INT);
+        $stmt->execute();      
         $num = $stmt->rowCount();
         
-        // link to create record form
         echo "<a href='create.php' class='btn btn-primary m-b-1em'>Create New Product</a>";        
         if($num>0){
         
             echo "<table class='table table-hover table-responsive table-bordered'>";//start table
- 
-            //creating our table heading
-            echo "<tr>";
+             echo "<tr>";
                 echo "<th>ID</th>";
                 echo "<th>Name</th>";
                 echo "<th>Description</th>";
@@ -50,33 +56,30 @@
             echo "</tr>";
             
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-                // extract row
-                // this will make $row['firstname'] to
-                // just $firstname only
                 extract($row);
-                 
-                // creating new table row per record
-                echo "<tr>";
+                    echo "<tr>";
                     echo "<td>{$id}</td>";
                     echo "<td>{$name}</td>";
                     echo "<td>{$description}</td>";
                     echo "<td>{$price}</td>";
                     echo "<td>";
-                        // read one record 
                         echo "<a href='read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
                          
-                        // we will use this links on next part of this post
                         echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
              
-                        // we will use this links on next part of this post
                         echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
                     echo "</td>";
                 echo "</tr>";
             }
         
-        // end table
         echo "</table>";
-            
+        $query = "SELECT COUNT(*) as total_rows FROM products";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $total_rows = $row['total_rows'];
+        $page_url="index.php?";
+        include_once "paging.php";
         }
         else{
             echo "<div class='alert alert-danger'>No records found.</div>";
