3d387d38a3a04f38511b5883372d74483fa049d5
aagudo <aagudo@cifpfbmoll.eu>
2020-11-23 13:44:38 +0100

    punto 12 acabado

--- phpCrud/read_one.php
+++ phpCrud/read_one.php
@@ -2,9 +2,7 @@
 <html>
 <head>
     <title>PDO - Read One Record - PHP CRUD Tutorial</title>
- 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
- 
 </head>
 <body>
     <div class="container">
@@ -17,7 +15,7 @@
         $id=isset($_GET['id']) ? $_GET['id'] : die('ERROR: Record ID not found.');
         include 'config/database.php';
         try {
-            $query = "SELECT id, name, description, price FROM products WHERE id = ? LIMIT 0,1";
+            $query = "SELECT id, name, description, price, image FROM products WHERE id = ? LIMIT 0,1";
             $stmt = $con->prepare( $query );
             $stmt->bindParam(1, $id);
             $stmt->execute();
@@ -25,6 +23,7 @@
             $name = $row['name'];
             $description = $row['description'];
             $price = $row['price'];
+            $image = htmlspecialchars($row['image'], ENT_QUOTES);
         }
         catch(PDOException $exception){
             die('ERROR: ' . $exception->getMessage());
@@ -44,6 +43,12 @@
             <td><?php echo htmlspecialchars($price, ENT_QUOTES);  ?></td>
         </tr>
         <tr>
+            <td>Image</td>
+            <td>
+            <?php echo $image ? "<img src='uploads/{$image}' style='width:300px;' />" : "No image found.";  ?>
+            </td>
+        </tr>
+        <tr>
             <td></td>
             <td>
                 <a href='index.php' class='btn btn-danger'>Back to read products</a>
@@ -54,6 +59,5 @@
     </div> 
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
 </body>
 </html>
